03ec7b506f97e91c9ff49031d6b04456b5a79ffd
1ww1s <ctop124@mail.ru>
2025-08-08 16:10:44 +0300

    update. добавил на 404 кнопку 'сообщить об ошибке'

--- src/widgets/notFound/ui/NotFound.tsx
+++ src/widgets/notFound/ui/NotFound.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 import classes from './notFound.module.scss'
+import Link from "next/link";
+import { MyButton } from "@/src/shared/ui/myButton";
 
 
 
@@ -11,7 +13,11 @@ export const NotFoundWidget: FC = () => {
             <h1 className={classes.status}>Ошибка 404</h1>
             <section className={classes.desc}>Неправильно набран адрес или такой страницы не существует</section>
             <section className={classes.inform}>
-                
+                <Link href={'https://t.me/maxim_jordan'}>
+                    <MyButton>
+                        Сообщить об ошибке
+                    </MyButton>
+                </Link>
             </section>
         </section>
     )
