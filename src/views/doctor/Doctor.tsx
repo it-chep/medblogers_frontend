dcd6fe2ea311ef851e38dc5f26ae77719c6e3087
1ww1s <ctop124@mail.ru>
2025-08-18 15:02:28 +0300

    update. loaderSpinner для деталки

--- src/views/doctor/Doctor.tsx
+++ src/views/doctor/Doctor.tsx
@@ -1,6 +1,7 @@
 import DoctorDetail from '@/src/widgets/doctorDetail'
 import classes from './doctor.module.scss'
 import { Suspense } from 'react'
+import { LoaderSpinner } from '@/src/shared/ui/loaderSpinner'
 
 interface IProps {
     slug: string
@@ -11,7 +12,7 @@ export default async function DoctorPage(props: IProps) {
     return (
         <section className={classes.page + ' wrapper_main'}>
             <main className={classes.main}>
-                <Suspense fallback={<>Loading...</>}>
+                <Suspense fallback={<section className={classes.loader}><LoaderSpinner /></section>}>
                     <DoctorDetail slug={props.slug} />
                 </Suspense>
             </main>
