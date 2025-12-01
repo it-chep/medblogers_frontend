2d1d329ea6b3aed1861a360f6e4dfdec4701051c
1ww1s <ctop124@mail.ru>
2025-12-01 15:07:27 +0300

    add. ХБ на страницах welcome

--- src/views/welcomeFreelancer/WelcomeFreelancer.tsx
+++ src/views/welcomeFreelancer/WelcomeFreelancer.tsx
@@ -1,4 +1,5 @@
 
+import { Breadcrumbs } from '@/src/widgets/breadcrumbs'
 import classes from './welcome.module.scss'
 import { WelcomeWidgetFreelancer } from '@/src/widgets/welcomeFreelancer'
 
@@ -7,6 +8,10 @@ export default function WelcomeFreelancerPage() {
 
     return (
         <section className={classes.page + ' wrapper_main'}>
+            <Breadcrumbs breadcrumbs={[
+                {path: '/helpers', label: 'Вернуться к базе'},
+                {path: '', label: 'Размещение в базе'},
+            ]} />
             <main className={classes.main}> 
                 <WelcomeWidgetFreelancer />
             </main>
