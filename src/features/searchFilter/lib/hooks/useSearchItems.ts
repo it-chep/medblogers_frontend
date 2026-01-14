236655350b381bbf40687cfad604ae09726cd8ef
1ww1s <ctop124@mail.ru>
2026-01-14 21:32:00 +0300

    update Вся миниатюра это ссылка в статьях. fix Предрасчет. add Выбранные фильтры

--- src/features/searchFilter/lib/hooks/useSearchItems.ts
+++ src/features/searchFilter/lib/hooks/useSearchItems.ts
@@ -1,9 +1,7 @@
-import { IItemFilter } from "@/src/entities/filter"
 import { useMemo } from "react"
 
 
-
-export function useSearchItems<T extends {name: string}> (value: string, items: T[]) {
+export function useSearchItems<T extends {name: string, selected?: boolean}> (value: string, items: T[]) {
     return useMemo(() => {
         return items.filter(item =>
                 item.name.toLocaleLowerCase().includes(value.toLocaleLowerCase())
---
+++ src/features/selectedFilter/ui/SelectedFilter.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { IFilter, IItemFilter, useFilterActions } from "@/src/entities/filter";
+import { FC, useMemo } from "react";
+import classes from './selectedFilter.module.scss'
+
+interface IProps {
+    items: IItemFilter[];
+    labelSlug: keyof Omit<IFilter, 'minSubscribers' | 'maxSubscribers'>;
+}
+
+export const SelectedFilter: FC<IProps> = ({items, labelSlug}) => {
+
+    const {setSelected} = useFilterActions()
+
+    const selectedItems = useMemo(() => {
+        return items.filter(item => item.selected)
+    }, [items])
+
+    const onDelete = (name: string) => {
+        setSelected({field: labelSlug, name, selected: false})
+    }
+
+    return (
+        <ul style={{display: selectedItems.length ? 'flex' : 'none'}} className={classes.list}>
+            {
+                selectedItems.map(item => 
+                    <li 
+                        key={item.name} 
+                        className={classes.item}
+                    >
+                        {item.name}
+                        <svg onClick={() => onDelete(item.name)} className={classes.close} width="16" height="16" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M25 7L7 25" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M25 25L7 7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </li>
+                )
+            }
+        </ul>
+    )
+}
