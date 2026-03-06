a72550e1c171ec403dce28aeb89c1819841fd51b
1ww1s <ctop124@mail.ru>
2026-03-07 00:25:13 +0300

    fix без логов

--- src/widgets/verifiedCookies/ui/VerifiedCookies.tsx
+++ src/widgets/verifiedCookies/ui/VerifiedCookies.tsx
@@ -15,7 +15,6 @@ export const VerifiedCookies: FC = () => {
     useEffect(() => {
         setIsMounted(true);
         const storedValue = localStorage.getItem('verifiedCookies');
-        console.log(storedValue)
         setVerified(Boolean(storedValue));
     }, []);
 
