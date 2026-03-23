f57f701fbf2a75650cef6f1b02a2de57d36b7e45
Maksim Necheporuk <mnecheporuka@ozon.ru>
2026-03-24 01:44:00 +0300

    Добавлен баннер

--- src/features/switchBanner/lib/helpers/Banners.tsx
+++ src/features/switchBanner/lib/helpers/Banners.tsx
@@ -1,29 +1,41 @@
-import { ISwitchBannerItem } from "../../model/types";
+import {ISwitchBannerItem} from "../../model/types";
 
 import Banner10 from '../assets/Banner10.png'
 import Banner10Mobile from '../assets/Banner10Mobile.png'
 import BannerVip from '../assets/BannerVip.png'
 import BannerVipMobile from '../assets/BannerVipMobile.png'
+import BannerNastav from '../assets/BannerNastav.png'
+import BannerNastavNobile from '../assets/BannerNastavMobile.png'
 
 export const Banners: ISwitchBannerItem[] = [
     {
-        link: 'http://localhost:3000/blogs',
-        toSite: true,
         url: Banner10.src
     },
     {
+        link: 'https://medblogers-base.ru/blogs/vip_card',
+        toSite: true,
         url: BannerVip.src
     },
+    {
+        link: 'https://medbloging.ru/',
+        toSite: false,
+        url: BannerNastav.src,
+    }
 ]
 
 
 export const BannersMobile: ISwitchBannerItem[] = [
     {
-        link: 'http://localhost:3000/blogs',
-        toSite: true,
         url: Banner10Mobile.src
     },
     {
+        link: 'https://medblogers-base.ru/blogs/vip_card',
+        toSite: true,
         url: BannerVipMobile.src
     },
+    {
+        link: 'https://medbloging.ru/',
+        toSite: false,
+        url: BannerNastavNobile.src,
+    }
 ]
