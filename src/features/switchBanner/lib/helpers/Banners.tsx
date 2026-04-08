e6568a090bc58d3a9c770bcb02ca3d0fc7940914
it-chep <mj0rdan@yandex.ru>
2026-04-08 23:46:28 +0300

    Добавлен баннер

--- src/features/switchBanner/lib/helpers/Banners.tsx
+++ src/features/switchBanner/lib/helpers/Banners.tsx
@@ -3,8 +3,16 @@ import BannerVip from '../assets/BannerVip.png'
 import BannerVipMobile from '../assets/BannerVipMobile.png'
 import BannerNastav from '../assets/BannerNastav.png'
 import BannerNastavNobile from '../assets/BannerNastavMobile.png'
+import BannerOffer from '../assets/BannerOffer.jpg'
+import BannerOfferMobile from '../assets/BannerOfferMobile.jpg'
+
 
 export const Banners: ISwitchBannerItem[] = [
+    {
+        link: 'https://medblogers-base.ru/promotional_offers',
+        toSite: true,
+        url: BannerOffer.src,
+    },
     {
         link: 'https://taplink.cc/readydoc',
         toSite: false,
@@ -19,6 +27,11 @@ export const Banners: ISwitchBannerItem[] = [
 
 
 export const BannersMobile: ISwitchBannerItem[] = [
+    {
+        link: 'https://medblogers-base.ru/promotional_offers',
+        toSite: true,
+        url: BannerOfferMobile.src,
+    },
     {
         link: 'https://taplink.cc/readydoc',
         toSite: false,
