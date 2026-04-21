5184e3fc4dfd0bc7a7950f3c5e2f4ef176bc5785
1ww1s <ctop124@mail.ru>
2026-04-22 00:15:52 +0300

    update ссылка на главную по лого в фиксированной шапке в мобиле

--- src/widgets/header/ui/fixedMenuMobile/FixedMenuMobile.tsx
+++ src/widgets/header/ui/fixedMenuMobile/FixedMenuMobile.tsx
@@ -1,6 +1,7 @@
 import { FC, PropsWithChildren, useCallback, useEffect, useRef } from "react";
 import classes from './fixedMenuMobile.module.scss'
 import { LogoMiniSvg } from "../../lib/assets/LogoSvg";
+import Link from "next/link";
 
 
 export const FixedMenuMobile: FC<PropsWithChildren> = ({children}) => {
@@ -8,7 +9,9 @@ export const FixedMenuMobile: FC<PropsWithChildren> = ({children}) => {
 
     return (
         <section className={classes.container}>
-            <LogoMiniSvg />
+            <Link href={'/'}>
+                <LogoMiniSvg />
+            </Link>
             <section className={classes.title}>
                 ЕДИНАЯ БАЗА ВРАЧЕЙ-БЛОГЕРОВ
             </section>
